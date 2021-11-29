20eb4f8dcbaa46c22f5180ccfec196d6a6c73eab
RitchielRez <ritchielrez@gmail.com>
2021-11-29 18:44:36 +0600

    Add neovim config

---
+++ .config/nvim/lua/init.lua
@@ -0,0 +1,5 @@
+require('plugins') -- Load up my plugins and their configs
+require('opts') -- Load my preffered options for neovim
+require('keymaps') -- Load up my keybindings/hotkeys/shortcuts
+
+
