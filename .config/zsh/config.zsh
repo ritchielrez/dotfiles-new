e8979b84275db627d342cc38e9f5d7e984df9109
RitchielRez <ritchielrez@gmail.com>
2021-12-10 20:11:01 +0600

    Trying out arrow history search

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -27,8 +27,16 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=1000
 setopt SHARE_HISTORY
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
+# bindkey '^[[A' up-line-or-search
+# bindkey '^[[B' down-line-or-search
 
 # if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   # source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
