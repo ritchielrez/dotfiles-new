37d3ded97eb2e3af42e228fb043aaaf8e76d9851
RitchielRez <ritchielrez@gmail.com>
2022-03-03 18:38:09 +0600

    feat(i3wm): autotiling

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -10,6 +10,11 @@ picom &
 # background 
 nitrogen --restore &
 
+# fibonacci layout in i3
+# checkout https://github.com/nwg-piotr/autotiling
+killall autotiling
+autotiling &
+
 # sxhkd
 killall sxhkd
 sxhkd -c $HOME/.config/i3/sxhkd/sxhkdrc &
