c3b70faedf7c182d94b865dcdac95ca3aa4dbb91
RitchielRez <ritchielrez@gmail.com>
2022-04-14 16:42:37 +0600

    fix(wezterm): font size to 15

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm';
 return {
   font = wezterm.font("FiraCode Nerd Font"),
-  font_size = 12,
+  font_size = 15,
   enable_tab_bar = false,
   color_scheme = "nord",
 }
