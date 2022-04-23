47456f59b7bd0dfd00ee013b259c93cda3bde7bb
RitchielRez <ritchielrez@gmail.com>
2022-04-23 20:35:39 +0600

    feat(wezterm): added new colorscheme
    added a new colorscheme for Gruvbox Material Dark

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ return {
   font = wezterm.font("UbuntuMono NF"),
   font_size = 17,
   enable_tab_bar = false,
-  color_scheme = "Gruvbox Dark",
+  color_scheme = "Gruvbox Material Dark",
+  color_scheme_dirs = {"~/.config/wezterm/colors/"},
 }
 
