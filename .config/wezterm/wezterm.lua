499919d4a87a6da3e832c58a14e02069a082dff1
RitchielRez <ritchielrez@gmail.com>
2022-04-24 23:41:47 +0600

    fix(wezterm): `line_height` added

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("UbuntuMono NF"),
-  font_size = 17,
+  font = wezterm.font("JetbrainsMono NF"),
+  font_size = 16,
   enable_tab_bar = false,
-  color_scheme = "Gruvbox Material Dark",
+  color_scheme = "nord",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
+  line_height = 1.5,
 }
 
