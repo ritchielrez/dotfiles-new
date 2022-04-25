270a1762b357863e190bdc747f1648266f0a7d7c
RitchielRez <ritchielrez@gmail.com>
2022-04-25 11:23:05 +0600

    fix(wezterm): removed `line_height`

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("FiraCode Nerd Font"),
+  font = wezterm.font("JetbrainsMono Nerd Font"),
   font_size = 16,
   enable_tab_bar = false,
   color_scheme = "nord",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
-  line_height = 1.5,
+  window_background_opacity = 0.95,
 }
 
