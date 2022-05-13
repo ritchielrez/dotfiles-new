b9d74677d2daea95d7a053f844d0ee2945a174cd
RitchielRez <ritchielrez@gmail.com>
2022-05-13 12:32:28 +0600

    feat(wezterm): changed to gruvbox material dark

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("JetbrainsMono Nerd Font"),
+  -- font = wezterm.font("JetbrainsMono Nerd Font"),
   font_size = 16,
   enable_tab_bar = false,
-  color_scheme = "nord",
+  color_scheme = "Gruvbox Material Dark",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
   window_background_opacity = 0.95,
 }
