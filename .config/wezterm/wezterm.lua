a703778283c3df4a6ffd8c4bf8fb90ba017388ad
RitchielRez <ritchielrez@gmail.com>
2022-06-13 22:52:44 +0600

    feat(wezterm): some small update
    system migration to MXLinux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("CaskaydiaCove Nerd Font"),
+  font = wezterm.font("JetbrainsMono Nerd Font"),
   font_size = 16,
   enable_tab_bar = false,
-  color_scheme = "Gruvbox Material Dark",
+  color_scheme = "nord_darker",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
-  window_background_opacity = 0.95,
+  window_background_opacity = 0.98,
 }
 
