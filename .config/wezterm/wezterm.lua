32c04d8c823a9981a4551971aa55dd5a1c374de9
RitchielRez <ritchielrez@gmail.com>
2022-06-14 15:44:25 +0600

    fix(wezterm): smaller font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm';
 return {
   font = wezterm.font("JetbrainsMono Nerd Font"),
-  font_size = 16,
+  font_size = 13,
   enable_tab_bar = false,
   color_scheme = "nord_darker",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
