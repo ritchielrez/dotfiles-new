db8132cd6d7a7978b47bc13a0862e60f0ef4b1e4
RitchielRez <ritchielrez@gmail.com>
2022-06-17 16:50:32 +0600

    feat(wezterm): migration to Fedora

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ return {
   font = wezterm.font("JetbrainsMono Nerd Font"),
   font_size = 13,
   enable_tab_bar = false,
-  color_scheme = "nord_darker",
+  color_scheme = "Gruvbox Material Dark",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
   window_background_opacity = 0.98,
   window_close_confirmation = "NeverPrompt",
