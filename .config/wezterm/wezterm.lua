4a03fa6c2458e37c206667d552f956ed3167966c
RitchielRez <ritchielrez@gmail.com>
2022-11-27 17:50:02 +0600

    feat(wezterm): add everforest theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,11 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("JetbrainsMono Nerd Font"),
-  -- font = wezterm.font_with_fallback({"nonicons", "JetbrainsMono Nerd Font"}),
+  -- font = wezterm.font("JetbrainsMono Nerd Font"),
   font_size = 13,
   enable_tab_bar = false,
-  color_scheme = "Gruvbox Material Dark",
+  color_scheme = "everforest",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
-  window_background_opacity = 0.98,
+  -- window_background_opacity = 0.98,
   window_close_confirmation = "NeverPrompt",
 }
 
