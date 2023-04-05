c98e147bb1d44caa491b2320f4ead943a019aa9e
RitchielRez <ritchielrez@gmail.com>
2023-04-05 20:03:20 +0600

    feat(wezterm): font change

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("Comic Mono"),
-  font_size = 14,
+  font = wezterm.font("FiraCode Nerd Font"),
+  font_size = 12,
   enable_tab_bar = false,
-  color_scheme = "everforest",
+  color_scheme = "Gruvbox Material Dark",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
   -- window_background_opacity = 0.98,
   window_close_confirmation = "NeverPrompt",
