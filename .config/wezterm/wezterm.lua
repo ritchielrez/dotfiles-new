73c311caf708c0b61f7d4af5e836ee258d5e40d1
RitchielRez <ritchielrez@gmail.com>
2023-04-07 18:03:54 +0600

    feat(wezterm): update colorscheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ return {
   font = wezterm.font("FiraCode Nerd Font"),
   font_size = 12,
   enable_tab_bar = false,
-  color_scheme = "Gruvbox Material Dark",
+  color_scheme = "nord",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
   -- window_background_opacity = 0.98,
   window_close_confirmation = "NeverPrompt",
