9c3ba3375522f6ece4fce79c26e05756f02a2dbf
RitchielRez <ritchielrez@gmail.com>
2023-04-18 22:46:43 +0600

    feat(wezterm): enable terminal opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
   enable_tab_bar = false,
   color_scheme = "Catppuccin Mocha",
   color_scheme_dirs = {"~/.config/wezterm/colors/"},
-  -- window_background_opacity = 0.98,
+  window_background_opacity = 0.8,
   window_close_confirmation = "NeverPrompt",
 }
 
