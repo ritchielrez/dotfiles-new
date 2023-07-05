d29c53d433b20e378f5224c78539b606c88e825c
RitchielRez <ritchielrez@gmail.com>
2023-07-05 12:58:48 +0600

    chore(wezterm): change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("FiraCode Nerd Font"),
+  font = wezterm.font("CaskaydiaCove Nerd Font"),
   font_size = 12,
   enable_tab_bar = false,
   color_scheme = "Catppuccin Mocha",
