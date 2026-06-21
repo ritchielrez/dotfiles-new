d6b90dcd167bbf9fe225250ad0179ae55b39aad1
RitchielRez <ritchielrez@gmail.com>
2026-06-21 11:13:55 -0400

    feat: add hypridle and hyprlock config

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -19,6 +19,7 @@ local menu = "rofi -show drun"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 hl.on("hyprland.start", function()
+	hl.exec_cmd("hypridle")
 	hl.exec_cmd("~/.azotebg-hyprland")
 	hl.exec_cmd("pipewire")
 end)
